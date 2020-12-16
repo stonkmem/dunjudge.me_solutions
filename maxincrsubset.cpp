603bf5d5a5418b6ae87c39b553a29316dbc10058
stonkmem <73430589+stonkmem@users.noreply.github.com>
2020-12-16 14:55:30 +0800

    Finish code for maxincrsubset.cpp

--- maxincrsubset.cpp
+++ maxincrsubset.cpp
@@ -1,6 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n;
 int main(){
-  ios_bas::sync_with_stdio(flase);
-  cin.tie(0)
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cin>>n;
+  int a[n], max[n];
+  for(int i=0; i<n; i++){
+    cin>>a[i];
+  }
+  for(int j=0; j<n; j++){
+    if(j!=0){
+      int biggest = a[j];
+      for(int l=j; l>=0; l--){
+        if(a[l]<biggest){
+          max[j]++;
+        }
+      }
+    }
+    else max[0] = 1;
+  }
+  sort(max, max+n);
+  reverse(max, max+n);
+  cout<<max[0];
 }
